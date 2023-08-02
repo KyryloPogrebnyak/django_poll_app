82f023f3c5b049dde7cd762bf77aac1b39c1f4d3
kyrylo <kpogrebnyak.28@gmail.com>
2023-08-02 21:55:06 +0200

    added test

--- polling/polls_app/views.py
+++ polling/polls_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 class IndexView(generic.ListView):
@@ -10,11 +11,21 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
     
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+            Return the last five published questions (not including those set to be
+            published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls_app/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
