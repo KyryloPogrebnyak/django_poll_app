407d60c3e29d27a62b60410b27962595c88b706b
kyrylo <kpogrebnyak.28@gmail.com>
2023-08-04 13:42:12 +0200

    added debug toolbar

--- polling/polling/urls.py
+++ polling/polling/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
 
 urlpatterns = [
     path("polls_app/", include("polls_app.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG: 
+    import debug_toolbar
+    urlpatterns += [re_path(r"^__debug__/", include("debug_toolbar.urls"))]
+
